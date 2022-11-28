c4602734b35b07982ed35e9eb6d806663c334960
Sabyna Maharani <sabyna.maharani@ui.ac.id>
2022-11-28 09:38:14 +0700

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -43,18 +43,17 @@ if not PRODUCTION:
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.staticfiles',  
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'main',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -141,20 +140,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # This is the directory for storing `collectstatic` results.
 # This shouldn't be included in your Git repository.
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 # You can use this directory to store project-wide static files.
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Make sure the directories exist to prevent errors when doing `collectstatic`.
-for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
-    directory.mkdir(exist_ok=True)
+# for directory in [*STATICFILES_DIRS, STATIC_ROOT]:
+#     directory.mkdir(exist_ok=True)
 
 # Enable compression and caching features of whitenoise.
 # You can remove this if it causes problems on your setup.
