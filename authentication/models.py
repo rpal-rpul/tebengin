346efdbc944827b2021f482ba0acff43a75aff0d
Sabyna Maharani <sabyna.maharani@ui.ac.id>
2022-12-02 23:53:46 +0700

    Add backend of dashboard driver

--- authentication/models.py
+++ authentication/models.py
@@ -1,9 +1,5 @@
-from audioop import reverse
-from email.policy import default
 from django.db import models
-from django.db.models.signals import post_save
 from django.contrib.auth.models import User
-from django.dispatch import receiver
 from polymorphic.models import PolymorphicModel
 
 
@@ -11,15 +7,20 @@ class Pengguna(PolymorphicModel):
     email = models.EmailField(max_length=254, blank=True, null=True)
     name = models.CharField(max_length=100)
     distance_from_campus = models.IntegerField(blank=True, null=True, default=0)
-    destination = models.CharField(max_length=100, blank=True, null=True,default="")
+    destination = models.CharField(max_length=100, blank=True, null=True, default="")
     USERNAME_FIELD = 'username'
     # rating = models.IntegerField(blank=True, null=True)
     # rating ntar diambil dari class rating
-    
-class Driver(Pengguna):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+class AvailableDateTime(models.Model):
     available_time_begin = models.DateTimeField(blank=True, null=True)
     available_time_end = models.DateTimeField(blank=True, null=True)
+
+
+class Driver(Pengguna):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    available_time = models.ManyToManyField(AvailableDateTime, blank=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True)
     fee_per_km = models.IntegerField(blank=True, null=True)
     license_plate = models.CharField(max_length=20, blank=True, null=True)
@@ -31,7 +32,3 @@ class Driver(Pengguna):
 class Customer(Pengguna):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     images = models.ImageField(upload_to='images/customer/', default='images/default.png')
-
-    
-    
-    
