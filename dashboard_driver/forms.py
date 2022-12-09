fad91deef2839d8ae5119410312c65b5002d1fec
Sabyna Maharani <sabyna.maharani@ui.ac.id>
2022-12-09 19:04:15 +0700

    Finish feature add available time

--- dashboard_driver/forms.py
+++ dashboard_driver/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 from authentication.models import AvailableDateTime
 
 
@@ -16,8 +17,18 @@ class AddAvailableTimeForm(forms.ModelForm):
             'available_time_end': DateTimeInput(),
         }
 
+    def clean(self):
+        data = dict(self.data.lists())
+        if len(data) != 0 and data['available_time_begin'][0] > data['available_time_end'][0]:
+            raise forms.ValidationError({"available_time_begin": "Time end can't precede time begin"})
+
     def __init__(self, *args, **kwargs):
         super(AddAvailableTimeForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'
-            field.widget.attrs['type'] = 'text'
+            if field_name == "available_time_begin":
+                field.widget.attrs['id'] = 'begin-time-column'
+                field.widget.attrs['name'] = 'btime-column'
+            else:
+                field.widget.attrs['id'] = 'end-time-column'
+                field.widget.attrs['name'] = 'etime-column'
--- dashboard_driver/views.py
+++ dashboard_driver/views.py
@@ -6,26 +6,57 @@ from authentication.models import Driver
 from dashboard_driver.forms import AddAvailableTimeForm
 from dashboard_driver.models import DashboardDriver, OrderStatus
 from django.forms.models import model_to_dict
-
+from main.views import check_user_role
+import json
+from django.core import serializers
+from datetime import datetime
 
 @csrf_exempt
 @login_required(login_url='/authentication/login/')
 def addAvailableTime(request):
     user = request.user
-    if request.method == 'GET':
-        return render(request, 'dashboard_driver/add_available_time.html', status=200)
+    form = AddAvailableTimeForm(request.POST)
+    is_driver = check_user_role(request.user)
+    context = {
+        'is_driver': is_driver,
+        'form': form
+    }
+    driver = Driver.objects.get(user=user)
 
     if request.method == 'POST':
-        form = AddAvailableTimeForm(request.POST)
-        driver = Driver.objects.get(user=user)
-
         if form.is_valid():
             form.save()
             driver.available_time.add(form.instance)
-            return JsonResponse(model_to_dict(form.instance), status=200, content_type="application/json")
-        return JsonResponse({"failed": "Failed to save available time"}, status=405)
-        
-    return JsonResponse({"failed": "Not using GET or POST method"}, status=405)
+            form_instance = form.instance
+            available_time_begin = form_instance.available_time_begin
+            available_time_end = form_instance.available_time_end
+
+            form_instance.available_time_begin = available_time_begin.strftime("%b. %d, %Y, %I:%M %p").replace(' 0', ' ')
+            form_instance.available_time_end = available_time_end.strftime("%b. %d, %Y, %I:%M %p").replace(' 0', ' ')
+            form_instance.available_time_begin = form_instance.available_time_begin.replace('PM', 'p.m.')
+            form_instance.available_time_begin = form_instance.available_time_begin.replace('AM', 'a.m.')
+            form_instance.available_time_end = form_instance.available_time_end.replace('PM', 'p.m.')
+            form_instance.available_time_end = form_instance.available_time_end.replace('AM', 'a.m.')
+
+            context = model_to_dict(form_instance)
+
+        elif form.errors:
+            context = {
+                "error_message": list(form.errors.as_data()["available_time_begin"][0])[0]
+            }
+
+        return JsonResponse(context, status=200)
+
+    if request.method == 'GET':
+        is_driver = check_user_role(request.user)
+        context = {'is_driver': is_driver, 'form': form,
+                   "models": driver.available_time.all()}
+        return render(request, 'dashboard_driver/add_available_time.html', context, status=200)
+
+    if request.is_ajax():
+
+        return JsonResponse()
+    return JsonResponse({"failed": "Not using right method"}, status=405)
 
 
 @csrf_exempt
