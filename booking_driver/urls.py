cf0114093e527ea05b0d34c929fc36a3fdfdb0f8
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2022-12-09 23:18:28 +0700

    Fix conflict

--- booking_driver/urls.py
+++ booking_driver/urls.py
@@ -2,16 +2,9 @@ from django.urls import path
 from .views import *
 
 app_name = 'booking_driver'
-from django.urls import path
-from .views import *
-
-app_name = 'booking_driver'
 
 urlpatterns = [
     path('', bookingDriver, name="booking-driver"),
-    path('create-order', create_order),
-    path('show-all-driver', show_all_driver, name="show-all-driver"),
-    path('', bookingDriver, name='bookingDriver'),
     path('create-order', create_order),
     path('show-all-driver', show_all_driver),
     path('show-filtered-driver', show_filtered_driver)
--- tebengin/settings.py
+++ tebengin/settings.py
@@ -94,20 +94,20 @@ WSGI_APPLICATION = 'tebengin.wsgi.application'
 
 DATABASES = {
     # uncomment for local development
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
     # uncomment for deployment
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('PGDATABASE'),
-        'USER': os.getenv('PGUSER'),
-        'PASSWORD': os.getenv('PGPASSWORD'),
-        'HOST': os.getenv('PGHOST'),
-        'PORT': os.getenv('PGPORT'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': os.getenv('PGDATABASE'),
+    #     'USER': os.getenv('PGUSER'),
+    #     'PASSWORD': os.getenv('PGPASSWORD'),
+    #     'HOST': os.getenv('PGHOST'),
+    #     'PORT': os.getenv('PGPORT'),
+    # }
 }
 
 
