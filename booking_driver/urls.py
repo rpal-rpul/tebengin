190b96b7c61ed6f24448de2c9d2f87f43fbfa41b
adnan-mkhtr <adnanmuchtar8e01@gmail.com>
2022-12-10 16:38:25 +0700

    Create booking driver

--- booking_driver/urls.py
+++ booking_driver/urls.py
@@ -5,7 +5,7 @@ app_name = 'booking_driver'
 
 urlpatterns = [
     path('', bookingDriver, name="booking-driver"),
-    path('create-order', create_order),
-    path('show-all-driver', show_all_driver),
+    path('create-order', create_order, name="order_driver"),
+    path('show-all-driver', show_all_driver, name="show_all_driver"),
     path('show-filtered-driver', show_filtered_driver)
 ]
