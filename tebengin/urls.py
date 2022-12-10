e590ad22912d4853ac57466a2d5a8ea0aa5b2ae8
ridjky.tegar <ridjky.tegar@ui.ac.id>
2022-12-10 18:33:36 +0700

    Pull from previous branch and fix conflicts

--- tebengin/urls.py
+++ tebengin/urls.py
@@ -1,5 +1,4 @@
 """tebengin URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
@@ -21,14 +20,7 @@ import authentication.urls as authentication
 import dashboard_driver.urls as dashboard_driver
 import respons_order.urls as respons_order
 import booking_driver.urls as booking_driver
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
 import profilepage.urls as profilepage
->>>>>>> c63688cbf4ac2b8bfb0bb9ea61637216ec16e189
-=======
-import profilepage.urls as profilepage
->>>>>>> fbf9b106606a5e733d22298a6c8ded8b21d8a993
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,17 +28,8 @@ urlpatterns = [
     path('authentication/', include(authentication)),
     path('dashboard-driver/', include(dashboard_driver)),
     path('respons-order/', include(respons_order)),
-<<<<<<< HEAD
-<<<<<<< HEAD
-    path('booking-driver/', include(booking_driver))
-=======
     path('booking-driver/', include(booking_driver)),
     path('profile/', include(profilepage)),
->>>>>>> c63688cbf4ac2b8bfb0bb9ea61637216ec16e189
-=======
-    path('booking-driver/', include(booking_driver)),
-    path('profile/', include(profilepage)),
->>>>>>> fbf9b106606a5e733d22298a6c8ded8b21d8a993
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
