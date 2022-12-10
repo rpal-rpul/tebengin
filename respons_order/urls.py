7c7bc2344789f0813de92931199da8681bab0c50
Sabyna Maharani <sabyna.maharani@ui.ac.id>
2022-12-10 16:28:13 +0700

    Implement dashboard driver and status order feature

--- respons_order/urls.py
+++ respons_order/urls.py
@@ -1,8 +1,9 @@
 app_name = 'respons_order'
 from django.urls import path
-from .views import accept_order, reject_order
+from .views import accept_order, reject_order, finish_order
 
 urlpatterns = [
     path('accept-order', accept_order, name="accept_order"),
-    path('reject-order', reject_order, name="reject_order")
+    path('reject-order', reject_order, name="reject_order"),
+    path('finish-order', finish_order, name="finish_order")
 ]
